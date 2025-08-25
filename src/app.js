9de19d2b5f54f4b7599ed857c6f402b21cc29c4c
francielleabreu <fran.abreu2@gmail.com>
2025-08-25 14:22:19 +0200

    teste

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// Função simples para somar dois números e exportar a função para teste
+function soma(a, b) {
+  return a + b;
+}
+
+module.exports = { soma };
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,3 +1,10 @@
-test('soma 2 + 2', () => {
-  expect(2 + 2).toBe(4);
+const { soma } = require('../src/app');
+
+test('soma 2 + 2 deve ser 4', () => {
+  expect(soma(2, 2)).toBe(4);
 });
+
+test('soma 5 + 3 deve ser 8', () => {
+  expect(soma(5, 3)).toBe(8);
+});
+
